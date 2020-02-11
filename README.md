40f9af75a13e433d2aa12cdf245ec5ef9f30607b
Lucien Greathouse <me@lpghatguy.com>
2020-02-10 23:47:16 -0800

    Docs and slight modernization

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rust on Teensy 3.6
+I'm reviving this project after a couple years of inactivity after hitting a brick wall trying to configure timers on the board. If all else fails, maybe I'll buy a Teensy 4.0 and see if there's more community support!
+
+## Requirements
+- Xargo
+- Teensy loader CLI
+- arm-none-eabi target and binutils
+
+Run `./build` to build and deploy to a connected device.
--- src/main.rs
+++ src/main.rs
@@ -2,98 +2,97 @@
 #![no_std]
 #![no_main]
 
-extern crate volatile;
-extern crate bit_field;
+use core::panic::PanicInfo;
 
+extern crate bit_field;
+extern crate volatile;
+
+mod mcg;
+mod osc;
 mod port;
 mod sim;
 mod watchdog;
-mod mcg;
-mod osc;
 
-extern {
-	fn _stack_top();
+extern "C" {
+    fn _stack_top();
 }
 
 #[link_section = ".vectors"]
 #[no_mangle]
-pub static _VECTORS: [unsafe extern fn(); 2] = [
-	_stack_top,
-	main,
-];
+pub static _VECTORS: [unsafe extern "C" fn(); 2] = [_stack_top, main];
 
 #[link_section = ".flashconfig"]
 #[no_mangle]
 pub static _FLASHCONFIG: [u8; 16] = [
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-	0xFF, 0xFF, 0xFF, 0xFF, 0xDE, 0xF9, 0xFF, 0xFF
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xDE, 0xF9, 0xFF, 0xFF,
 ];
 
-#[lang = "panic_fmt"]
-#[no_mangle]
-pub extern "C" fn rust_begin_unwind(_msg: core::fmt::Arguments, _file: &'static str, _line: u32) -> ! {
-	loop {};
+#[panic_handler]
+fn teensy_panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
 
 fn delay(cycles: usize) {
-	unsafe {
-		for i in 0..cycles {
-			asm!("nop" : : : "memory");
-		}
-	}
+    unsafe {
+        for i in 0..cycles {
+            asm!("nop" : : : "memory");
+        }
+    }
 }
 
 fn yep(gpio: &mut port::Gpio) {
-	gpio.high();
-	delay(1_200_000);
-	gpio.low();
+    gpio.high();
+    delay(1_200_000);
+    gpio.low();
 }
 
 fn nope(gpio: &mut port::Gpio) {
-	gpio.high();
-	delay(172_000);
-	gpio.low();
+    gpio.high();
+    delay(172_000);
+    gpio.low();
 
-	delay(640_000);
+    delay(640_000);
 
-	gpio.high();
-	delay(172_000);
-	gpio.low();
+    gpio.high();
+    delay(172_000);
+    gpio.low();
 }
 
 #[no_mangle]
-pub extern fn main() {
-	let (wdog, sim, pin, osc, mcg) = unsafe {(
-		watchdog::Watchdog::new(),
-		sim::Sim::new(),
-		port::Port::new(port::PortName::C).pin(5),
-		osc::Osc::new(),
-		mcg::Mcg::new(),
-	)};
+pub extern "C" fn main() {
+    let (wdog, sim, pin, osc, mcg) = unsafe {
+        (
+            watchdog::Watchdog::new(),
+            sim::Sim::new(),
+            port::Port::new(port::PortName::C).pin(5),
+            osc::Osc::new(),
+            mcg::Mcg::new(),
+        )
+    };
 
-	wdog.disable();
+    wdog.disable();
 
-	osc.enable(10);
+    // osc.enable(10);
 
-	sim.enable_clock(sim::Clock::PortC);
+    sim.enable_clock(sim::Clock::PortC);
 
-	sim.set_dividers(1, 2, 3);
+    // sim.set_dividers(1, 2, 3);
 
-	mcg.fei_to_pee_120mhz();
+    // mcg.fei_to_pee_120mhz();
 
-	let mut gpio = pin.make_gpio();
+    let mut gpio = pin.make_gpio();
+    gpio.output();
+    gpio.high();
 
-	gpio.output();
+    // {
+    //     use bit_field::BitField;
 
-	{
-		use bit_field::BitField;
+    //     if mcg.c1.read().get_bits(3..6) == 0b100 {
+    //         yep(&mut gpio);
+    //     } else {
+    //         nope(&mut gpio);
+    //     }
+    // }
 
-		if mcg.c1.read().get_bits(3..6) == 0b100 {
-			yep(&mut gpio);
-		} else {
-			nope(&mut gpio);
-		}
-	}
-
-	loop {}
+    loop {}
 }
